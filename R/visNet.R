8be12be8c657e2f100e76c91567320b7b9ad2970
aliciamrich <aliciamrich@gmail.com>
2025-04-14 14:23:44 -0500

    ped vis subnucs in progress

--- R/visNet.R
+++ R/visNet.R
@@ -1,4 +1,3 @@
-
 ped_visNodes <- function(studbook, pedigree) {
   subjects <- nodes_subjects(studbook = studbook,
                              pedigree = pedigree) %>%
@@ -18,49 +17,28 @@ ped_visNodes <- function(studbook, pedigree) {
   return(nodes)
 }
 
-ped_ndf <- function(studbook, pedigree) {
-  nodes <- ped_visNodes(studbook = studbook,
-                        pedigree = pedigree)
-  ndf <- create_node_df(
-    n     = max(nodes$id),
-    id    = nodes$id,
-    label = nodes$label,
-    level = nodes$level,
-    group = nodes$group,
-    title = nodes$title
-  )
-  return(ndf)
-}
 
 ped_visEdges <- function(studbook, pedigree) {
+  colors <- set_colors()
   edges <- ped_edges(studbook = studbook,
                      pedigree = pedigree) %>%
+    mutate(color = if_else(rel %in% c("children", "subnucs"),
+                           colors[["u"]],
+                           color)) %>%
     select(from,
            to,
            color,
-           rel) %>%
-    mutate(width      = 1.5)
+           rel)
   return(edges)
 }
 
 
-ped_edf <- function(studbook, pedigree) {
-  edges <- ped_visEdges(studbook = studbook,
-                     pedigree = pedigree)
-  edf <- create_edge_df(
-    from  = edges$from,
-    to    = edges$to,
-    color = edges$color,
-    width = edges$width,
-    rel   = edges$rel
-  )
-  return(edf)
-}
-
 pedNode_visGroup <- function(graph, groupname) {
   path <- "https://rich-molecular-health-lab.github.io/zoopop/inst/icons/"
   file <- paste0(path, groupname, ".png")
-  if (groupname == "connector") { size <- 5 } else { size <- 40 }
+  if (groupname %in% c("connector", "offspring", "parents")) {
+    size <- 5
+    } else { size <- 40 }
   visGroups(graph     = graph,
             groupname = groupname,
             shape     = "image",
@@ -80,6 +58,97 @@ ped_visGroups <- function(graph) {
     pedNode_visGroup(               groupname = "undetermined"       ) %>%
     pedNode_visGroup(               groupname = "offspring"          ) %>%
     pedNode_visGroup(               groupname = "parents"            )
+
+}
+
+ped_groupIcons <- function() {
+  colors <- set_colors()
+  big    <- 75
+  med    <- 60
+  small  <- 15
+  icons <- list(
+    undetermined        = list(code  = "f219",
+                               size  = big,
+                               color = colors[["u"]]),
+    female_included     = list(code  = "f111",
+                               size  = big,
+                               color = colors[["f"]]),
+    female_excluded     = list(code  = "f056",
+                               size  = big,
+                               color = colors[["f"]]),
+    female_deceased     = list(code  = "f057",
+                               size  = med,
+                               color = colors[["f"]]),
+    female_hypothetical = list(code  = "f47e",
+                               size  = med,
+                               color = colors[["f"]]),
+      male_included     = list(code  = "f0c8",
+                               size  = big,
+                               color = colors[["m"]]),
+      male_excluded     = list(code  = "f146",
+                               size  = big,
+                               color = colors[["m"]]),
+      male_deceased     = list(code  = "f2d3",
+                               size  = med,
+                               color = colors[["m"]]),
+      male_hypothetical = list(code  = "f0fd",
+                               size  = med,
+                               color = colors[["m"]]),
+            offspring = list(code  = "f22d",
+                             size  = small,
+                             color = colors[["u"]]),
+            parents   = list(code  = "f22d",
+                             size  = small,
+                             color = colors[["emp"]])
+  )
+}
+
+ped_visIcons <- function(graph) {
+  icons  <- ped_groupIcons()
+  dimmed <- 0.8
+    visGroups(graph     = graph,
+              groupname = "female_included",
+              shape     = "icon",
+              icon      = icons[["female_included"]],
+              shadow    = TRUE) %>%
+    visGroups(groupname = "female_excluded",
+              shape     = "icon",
+              icon      = icons[["female_excluded"]],
+              opacity   = dimmed) %>%
+    visGroups(groupname = "female_deceased",
+              shape     = "icon",
+              icon      = icons[["female_deceased"]],
+              opacity   = dimmed) %>%
+      visGroups(groupname = "female_hypothetical",
+                shape     = "icon",
+                icon      = icons[["female_hypothetical"]],
+                opacity   = dimmed) %>%
+      visGroups(groupname = "male_included",
+                shape     = "icon",
+                icon      = icons[["male_included"]],
+                shadow    = TRUE) %>%
+      visGroups(groupname = "male_excluded",
+                shape     = "icon",
+                icon      = icons[["male_excluded"]],
+                opacity   = dimmed) %>%
+      visGroups(groupname = "male_deceased",
+                shape     = "icon",
+                icon      = icons[["male_deceased"]],
+                opacity   = dimmed) %>%
+      visGroups(groupname = "male_hypothetical",
+                shape     = "icon",
+                icon      = icons[["male_hypothetical"]],
+                opacity   = dimmed) %>%
+      visGroups(groupname = "offspring",
+                shape     = "icon",
+                icon      = icons[["offspring"]]) %>%
+      visGroups(groupname = "parents",
+                shape     = "icon",
+                icon      = icons[["parents"]]) %>%
+      visGroups(groupname = "undetermined",
+                shape     = "icon",
+                icon      = icons[["undetermined"]]) %>%
+    addFontAwesome()
 }
 
 ped_visNet <- function(studbook, pedigree) {
@@ -92,10 +161,23 @@ ped_visNet <- function(studbook, pedigree) {
     studbook = studbook,
     pedigree = pedigree
     )
-  graph <- visNetwork(nodes = nodes, edges = edges) %>%
-    ped_visGroups() %>%
-    visInteraction(tooltipDelay = 7) %>%
-    visIgraphLayout(layout = "layout_with_sugiyama")
+  graph <- visNetwork(nodes  = nodes,
+                      edges  = edges,
+                      width  = "100%") %>%
+    visEdges(width = 1.5) %>%
+    ped_visIcons() %>%
+    visInteraction(tooltipDelay = 10,
+                   tooltipStyle = "visibility:hidden")
   return(graph)
 }
 
+ped_visTree <- function(studbook, pedigree) {
+  ped_visNet(studbook = studbook,
+             pedigree = pedigree) %>%
+  visHierarchicalLayout(
+    levelSeparation = 400,
+    nodeSpacing     = 150,
+    sortMethod      = "directed"
+  ) %>%
+    visPhysics(enabled = FALSE)
+}
